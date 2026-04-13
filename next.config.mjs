07ebb1e0829a31326ad23437a8e24480cd151787
Paninche <32and8@gmail.com>
2026-04-13 11:23:37 -0400

    Remove www redirect causing redirect loop

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'www.necesitoya.app' }],
-        destination: 'https://necesitoya.app/:path*',
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
