d1da2c1bbca1de3dd3dbac34eb1c697912fd6451
Paninche <32and8@gmail.com>
2026-04-13 10:26:01 -0400

    Add www to non-www redirect for SEO

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.necesitoya.app' }],
+        destination: 'https://necesitoya.app/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
